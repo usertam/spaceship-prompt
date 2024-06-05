5b82eb25ad87a6c0b62c0681794bd0332b285f7e
Samuel Tam <heiyiutam@gmail.com>
2024-06-06 03:15:13 +0800

    nix_shell.zsh: customize for new nix shell

--- sections/nix_shell.zsh
+++ sections/nix_shell.zsh
@@ -10,10 +10,11 @@
 
 SPACESHIP_NIX_SHELL_SHOW="${SPACESHIP_NIX_SHELL_SHOW=true}"
 SPACESHIP_NIX_SHELL_ASYNC="${SPACESHIP_NIX_SHELL_ASYNC=false}"
+SPACESHIP_NIX_SHELL_VERSION="${SPACESHIP_NIX_SHELL_VERSION=false}"
 SPACESHIP_NIX_SHELL_PREFIX="${SPACESHIP_NIX_SHELL_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"}"
 SPACESHIP_NIX_SHELL_SUFFIX="${SPACESHIP_NIX_SHELL_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"}"
 SPACESHIP_NIX_SHELL_SYMBOL="${SPACESHIP_NIX_SHELL_SYMBOL="❄ "}"
-SPACESHIP_NIX_SHELL_COLOR="${SPACESHIP_NIX_SHELL_COLOR="yellow"}"
+SPACESHIP_NIX_SHELL_COLOR="${SPACESHIP_NIX_SHELL_COLOR="blue"}"
 
 # ------------------------------------------------------------------------------
 # Section
@@ -23,12 +24,12 @@ SPACESHIP_NIX_SHELL_COLOR="${SPACESHIP_NIX_SHELL_COLOR="yellow"}"
 spaceship_nix_shell() {
   [[ $SPACESHIP_NIX_SHELL_SHOW == false ]] && return
 
-  [[ -z "$IN_NIX_SHELL" ]] && return
+  [[ -z "$IN_NIX_SHELL" ]] && ! (echo "$PATH" | grep -q '/nix/store') && return
 
-  if [[ -z "$name" || "$name" == "" ]] then
-    display_text="$IN_NIX_SHELL"
+  if [[ $SPACESHIP_NIX_SHELL_VERSION == true ]]; then
+    display_text="$(echo "$PATH" | grep -Po '/nix/store.*?/bin' | uniq | sed ':a; s+/.\{42\}-++g; s+/bin++g; s/\n/, /g; N; ba;')"
   else
-    display_text="$IN_NIX_SHELL ($name)"
+    display_text="$(echo "$PATH" | grep -Po '/nix/store.*?/bin' | uniq | sed ':a; s+/.\{42\}-++g; s+/bin++g; s/-[0-9][0-9.]*//g; s/\n/, /g; N; ba;')"
   fi
 
   # Show prompt section
